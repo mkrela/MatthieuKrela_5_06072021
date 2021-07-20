a4c2c6b6759916a94d22e5c78e52c55832321d8f
mkrela <77677471+mkrela@users.noreply.github.com>
2021-07-20 22:28:44 +0200

    Index quasi fini

---
+++ index.js
@@ -0,0 +1,60 @@
+main();
+
+function main() {
+  getArticles();
+}
+
+function getArticles() {
+  fetch("http://localhost:3000/api/teddies")
+    .then(function (res) {
+      return res.json();
+    })
+    .catch((error) => {
+      let productsContainer = document.querySelector(".products-container");
+      productsContainer.innerHTML = "Le back-end semble ne pas fonctionner";
+      productsContainer.style.textAlign = "center";
+      productsContainer.style.padding = "40vh 0";
+    })
+
+    //   Disposition des données relatives aux articles dans le DOM
+    .then(function (resultatAPI) {
+      const articles = resultatAPI;
+    for (let article in articles) {
+        let productCard = document.createElement("div");
+        document.querySelector(".products").appendChild(productCard);
+        productCard.classList.add("product");
+
+        let productLink = document.createElement("a");
+        productCard.appendChild(productLink);
+        productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+        productLink.classList.add("stretched-link");
+
+        let productImgDiv = document.createElement("div");
+        productLink.appendChild(productImgDiv);
+        productImgDiv.classList.add("product__img");
+
+        let productImg = document.createElement("img");
+        productImgDiv.appendChild(productImg);
+        productImg.src = resultatAPI[article].imageUrl;
+
+        let productInfosDiv = document.createElement("div");
+        productLink.appendChild(productInfosDiv);
+        productInfosDiv.classList.add("product__infos");
+
+        let productInfoTitle = document.createElement("div");
+        productInfosDiv.appendChild(productInfoTitle);
+        productInfoTitle.innerHTML = resultatAPI[article].name;
+
+        let productInfoPrice = document.createElement("div");
+        productInfosDiv.appendChild(productInfoPrice);
+        productInfoPrice.classList.add("product__infos__price");
+
+        // Affichage du prix en eur
+        resultatAPI[article].price = resultatAPI[article].price / 100;
+        productInfoPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
+          style: "currency",
+          currency: "EUR",
+        }).format(resultatAPI[article].price);
+      }
+    });
+}
